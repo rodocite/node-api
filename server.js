d1695a0def93fb91f52765e882452fa7657cc113
Rodolfo Yabut <rodolfoyabut@gmail.com>
2016-01-03 02:15:01 -0800

    Change endpoints to be more RESTful * Used Express built-in routes for REST instead of manually routing

--- server.js
+++ server.js
@@ -7,56 +7,56 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
-var router = express.Router();
 
 // Endpoints
-router.route('/get')
-  .get((req, res) => {
-    Name.findAll({
-      attributes: ['id', 'name']
-    })
-    .then(names => res.json({data: names}));
-  });
-
-router.route('/post')
-  // TODO: Handling req.body on null/error submissions
-  .post((req, res) => {
-    var TODO = req.body.name || 'default';
-    Name.create({ name: TODO }).then(post => {
-      res.json(post);
-    });
-  });
-
-router.route('/update')
-  .post((req, res) => {
-    var newName = req.body.name;
-    var id = req.body.id;
-
-    Name.findById(id).then(name => {
-      if(name) {
-        name.updateAttributes({ name: newName });
-      } else {
-        res.json('Could not find record.')
-      }
-    });
-  });
-
-router.route('/delete')
-  .post((req, res) => {
-    var newName = req.body.name;
-    var id = req.body.id;
-
-    Name.findById(id).then(name => {
-      if(name) {
-        name.destroy();
-        res.json('Record successfully deleted.');
-      } else {
-        res.json('Could not find record.');
-      }
-    });
+app.get('/names', (req, res) => {
+  Name.findAll({
+    attributes: ['id', 'name']
   })
+  .then(names => res.json({data: names}));
+})
 
+app.get('/names/:id', (req, res) => {
+  var id = req.params.id;
 
-app.use('/api', router);
+  Name.findById(id)
+  .then(name => res.json(name));
+})
+
+app.post('/names', (req, res) => {
+  // TODO: Better req.body handling
+
+  var TODO = req.body.name || 'default';
+  Name.create({ name: TODO }).then(post => {
+    res.json(post);
+  });
+});
+
+app.put('/names/:id', (req, res) => {
+  var newName = req.body.name;
+  var id = req.params.id;
+
+  Name.findById(id).then(name => {
+    if(name) {
+      name.updateAttributes({ name: newName });
+    }
+
+    res.json(name);
+  });
+});
+
+app.delete('/names/:id', (req, res) => {
+  var newName = req.body.name;
+  var id = req.params.id;
+
+  Name.findById(id).then(name => {
+    if(name) {
+      name.destroy();
+    }
+  });
+
+  res.status(200);
+})
+
 app.listen(port);
 console.log('Using port ' + port);
